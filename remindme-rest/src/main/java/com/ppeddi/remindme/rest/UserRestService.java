acc262c1fac7aaa81773a07948ca43ee871142bd
Praveen Peddi <ppeddi@constantcontact.com>
2012-06-14 12:54:43 -0400

    fix rest endpoint

--- remindme-rest/src/main/java/com/ppeddi/remindme/rest/UserRestService.java
+++ remindme-rest/src/main/java/com/ppeddi/remindme/rest/UserRestService.java
@@ -3,11 +3,13 @@
  */
 package com.ppeddi.remindme.rest;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 
 import com.ppeddi.remindme.domain.User;
 
@@ -16,6 +18,8 @@ import com.ppeddi.remindme.domain.User;
  * 
  */
 @Path("/v1/user")
+@Consumes("application/json")
+@Produces("application/json")
 public interface UserRestService {
 
 	@POST
